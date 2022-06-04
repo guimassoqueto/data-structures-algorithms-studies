5dfbd013033f2103a82468f546f22ebb4656dd00
Guilherme A. Massoqueto <gui.massoqueto@gmail.com>
2022-06-04 09:08:16 -0300

    covering segments

--- ASSIGNMENTS/PART1/week3/5_covering_segments.py
+++ ASSIGNMENTS/PART1/week3/5_covering_segments.py
@@ -1,21 +1,33 @@
-# Uses python3
+#! /usr/bin/python3
 import sys
 from collections import namedtuple
 
 Segment = namedtuple('Segment', 'start end')
 
 def optimal_points(segments):
+    number_of_segments = len(segments)
     points = []
     #write your code here
-    for s in segments:
-        points.append(s.start)
-        points.append(s.end)
-    return points
+    for s in segments: points += list(range(s.start, s.end + 1))
+    
+    result = []
+    _dict = {}
+    for i in points:
+        _dict.setdefault(i, 0)
+        _dict[i] += 1
+
+    max_count = max(_dict.values())
+    for k, v in _dict.items(): 
+        if v == max_count:
+            result.append(k) 
+    
+    print(points)
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *data = map(int, input.split())
     segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
     points = optimal_points(segments)
-    print(len(points))
-    print(*points)
+    #print(len(points))
+    #print(*points)
---
+++ ASSIGNMENTS/PART1/week3/test.py
@@ -0,0 +1,7 @@
+
+
+lists = list(range(1,4)) + list(range(2,6)) + list(range(3,7))
+
+
+
+print(_dict, results)
